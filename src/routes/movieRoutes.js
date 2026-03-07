602b6eae9e62a219e52daccd154ea4650a1b4d1e
rmishra00 <riya@nostacklabs.com>
2026-03-07 21:35:37 +0530

    add role based access

--- src/routes/movieRoutes.js
+++ src/routes/movieRoutes.js
@@ -2,13 +2,15 @@ const express = require('express');
 const router = express.Router();
 const {addMovie, getMovies, searchMovies, streamMovie, uploadMovieVideo, updateMovie, deleteMovie} = require('../controllers/movieController');
 const authMiddleware = require('../middleware/authMiddleware');
+const adminMiddleware = require('../middleware/adminMiddleware');
+
 const upload = require('../config/multer')
-router.post('/',authMiddleware, addMovie);
-router.put('/:id', authMiddleware, updateMovie);
-router.delete('/:id', authMiddleware, deleteMovie);
+router.post('/',authMiddleware, adminMiddleware, addMovie);
+router.put('/:id', authMiddleware,adminMiddleware, updateMovie);
+router.delete('/:id', authMiddleware,adminMiddleware, deleteMovie);
 router.get('/', getMovies);
 router.get('/search', searchMovies);
-router.post('/:id/upload', authMiddleware, upload.single('video'), uploadMovieVideo)
+router.post('/:id/upload', authMiddleware, adminMiddleware, upload.single('video'), uploadMovieVideo)
 router.get('/:id/stream', streamMovie);
 
 module.exports = router;
