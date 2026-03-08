7d51417123787965f570b4e6d5c30805f080d13d
rmishra00 <riya@nostacklabs.com>
2026-03-08 12:02:38 +0530

    add request validation

--- src/routes/movieRoutes.js
+++ src/routes/movieRoutes.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const {addMovie, getMovies, searchMovies, streamMovie, uploadMovieVideo, updateMovie, deleteMovie} = require('../controllers/movieController');
 const authMiddleware = require('../middleware/authMiddleware');
 const adminMiddleware = require('../middleware/adminMiddleware');
+const validateRequest = require("../middleware/validateRequest");
+const movieSchema = require("../validators/movieValidator");
 
 const upload = require('../config/multer')
-router.post('/',authMiddleware, adminMiddleware, addMovie);
+router.post('/',authMiddleware, adminMiddleware,  validateRequest(movieSchema), addMovie);
 router.put('/:id', authMiddleware,adminMiddleware, updateMovie);
 router.delete('/:id', authMiddleware,adminMiddleware, deleteMovie);
 router.get('/', getMovies);
